ce9ab518c5b178438f2ee7bf9360696ba0bdf8f1
ankhzet <ankhzet@gmail.com>
2014-03-03 19:19:02 +0200

    Updated TechTree specs
    * AZRTechTreeSpec
    + AZRTechnologySpec
    + AZRTechIterationSpec

--- RealmzTests/AZRTechTreeSpec.m
+++ RealmzTests/AZRTechTreeSpec.m
@@ -25,18 +25,23 @@ describe(@"Testing tech tree", ^{
 	});
 
 	it(@"should manage techs", ^{
-		id tech1 = [AZRTechnology mock];
+		id tech1 = [AZRTechnology nullMock];
 		[[tech1 should] receive:@selector(name) andReturn:techName1 withCountAtLeast:0];
-		id tech2 = [AZRTechnology mock];
+		id tech2 = [AZRTechnology nullMock];
 		[[tech2 should] receive:@selector(name) andReturn:techName2 withCountAtLeast:0];
+		id tech3 = [AZRTechnology nullMock];
+		[[tech3 should] receive:@selector(name) andReturn:techName3 withCountAtLeast:0];
 
 		[techTree addTech:tech1];
 		[techTree addTech:tech2];
+		[techTree addTech:tech3];
 
 		[[[techTree techNamed:techName1] shouldNot] beNil];
 		[[[techTree techNamed:techName2] shouldNot] beNil];
+		[[[techTree techNamed:techName3] shouldNot] beNil];
 		[[[techTree techNamed:techName1] should] equal:tech1];
 		[[[techTree techNamed:techName2] should] equal:tech2];
+		[[[techTree techNamed:techName3] should] equal:tech3];
 		[[[techTree techNamed:techName1] shouldNot] equal:tech2];
 
 		AZRTechnology *removed = [techTree removeTech:techName1];
@@ -44,19 +49,40 @@ describe(@"Testing tech tree", ^{
 		[[removed should] equal:tech1];
 		[[[techTree techNamed:techName1] should] beNil];
 
-		[[techTree.technologies should] haveCountOf:1];
+		[[techTree.technologies should] haveCountOf:2];
 		[techTree addTech:tech1];
 		[techTree addTech:tech1];
 		[techTree addTech:tech2];
-		[[techTree.technologies should] haveCountOf:2];
+		[[techTree.technologies should] haveCountOf:3];
+	});
+
+	it(@"should fetch tech dependencies", ^{
+		AZRTechnology *tech1 = [techTree techNamed:techName1];
+		AZRTechnology *tech2 = [techTree techNamed:techName2];
+		AZRTechnology *tech3 = [techTree techNamed:techName3];
+
+		[[tech2 should] receive:@selector(isDependentOf:) andReturn:theValue(YES) withArguments:tech1];
+		[[tech3 should] receive:@selector(isDependentOf:) andReturn:theValue(YES) withArguments:tech2];
+
+		NSArray *dependencies1 = [techTree fetchTechDependencies:tech1];
+		NSArray *dependencies2 = [techTree fetchTechDependencies:tech2];
+		NSArray *dependencies3 = [techTree fetchTechDependencies:tech3];
+		[[dependencies1 shouldNot] beNil];
+		[[dependencies2 shouldNot] beNil];
+		[[dependencies3 shouldNot] beNil];
+		[[dependencies1 should] haveCountOf:1];
+		[[theValue([dependencies1 containsObject:tech2]) should] beYes];
+		[[theValue([dependencies1 containsObject:tech3]) should] beNo];
+		[[theValue([dependencies2 containsObject:tech1]) should] beNo];
+		[[theValue([dependencies2 containsObject:tech3]) should] beYes];
+		[[theValue([dependencies3 containsObject:tech1]) should] beNo];
+		[[theValue([dependencies3 containsObject:tech2]) should] beNo];
 	});
 
 	it(@"should fetch techs with different states", ^{
 		AZRTechnology *tech1 = [techTree techNamed:techName1];
 		AZRTechnology *tech2 = [techTree techNamed:techName2];
-		AZRTechnology *tech3 = [AZRTechnology mock];
-		[[tech3 should] receive:@selector(name) andReturn:techName3 withCountAtLeast:0];
-		[techTree addTech:tech3];
+		AZRTechnology *tech3 = [techTree techNamed:techName3];
 
 		[[tech1 should] receive:@selector(isUnavailable) andReturn:theValue(NO) withCountAtLeast:0];
 		[[tech2 should] receive:@selector(isUnavailable) andReturn:theValue(YES) withCountAtLeast:0];
@@ -96,14 +122,8 @@ describe(@"Testing tech tree", ^{
 		AZRTechnology *tech2 = [techTree techNamed:techName2];
 		AZRTechnology *tech3 = [techTree techNamed:techName3];
 
-		[[tech1 should] receive:@selector(calcAvailability)];
-		[[tech2 should] receive:@selector(calcAvailability)];
-		[[tech3 should] receive:@selector(calcAvailability)];
-
 		NSTimeInterval lastTick = [NSDate timeIntervalSinceReferenceDate];
-		// techs in "normal" (not inProcess) state should NOT be processed
-		[[tech1 shouldNot] receive:@selector(process:) withArguments:theValue(lastTick)];
-		// techs in "inProcess" (even if "unavailable") state SHOULD be processed
+		[[tech1 should] receive:@selector(process:) withArguments:theValue(lastTick)];
 		[[tech2 should] receive:@selector(process:) withArguments:theValue(lastTick)];
 		[[tech3 should] receive:@selector(process:) withArguments:theValue(lastTick)];
 
